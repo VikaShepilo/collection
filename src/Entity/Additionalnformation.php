424e8aa13c3517e5dc77500b573d11471cd861cd
VikaShepilo <shepilovika@gmail.com>
2022-01-17 22:33:22 +0300

    Created: new class

--- src/Entity/Additionalnformation.php
+++ src/Entity/Additionalnformation.php
@@ -2,15 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\CollectionsRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Repository\AdditionalnformationRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=CollectionsRepository::class)
+ * @ORM\Entity(repositoryClass=AdditionalnformationRepository::class)
  */
-class Collections
+class Additionalnformation
 {
     /**
      * @ORM\Id
@@ -18,36 +16,6 @@ class Collections
      * @ORM\Column(type="integer")
      */
     private $id;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $id_author;
-
-    /**
-     * @ORM\Column(type="string", length=50)
-     */
-    private $name;
-
-    /**
-     * @ORM\Column(type="string", length=225)
-     */
-    private $description;
-
-    /**
-     * @ORM\Column(type="string", length=30)
-     */
-    private $topic;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $img;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Item::class, mappedBy="collection", orphanRemoval=true)
-     */
-    private $items;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
@@ -123,6 +91,11 @@ class Collections
      * @ORM\Column(type="boolean", nullable=true)
      */
     private $abultContent;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
     public function getFees(): ?int
     {
@@ -300,120 +273,6 @@ class Collections
     public function setAbultContent(?bool $abultContent): self
     {
         $this->abultContent = $abultContent;
-
-        return $this;
-    }
-
-    public function __construct()
-    {
-        $this->items = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-
-    public function getIdAuthor(): ?int
-    {
-        return $this->id_author;
-    }
-
-    public function setIdAuthor(int $id_author): self
-    {
-        $this->id_author = $id_author;
-
-        return $this;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getTopic(): ?string
-    {
-        return $this->topic;
-    }
-
-    public function setTopic(string $topic): self
-    {
-        $this->topic = $topic;
-
-        return $this;
-    }
-
-    public function getImg(): ?string
-    {
-        return $this->img;
-    }
-
-    public function setImg(?string $img): self
-    {
-        $this->img = $img;
-
-        return $this;
-    }
-
-    public function __toString()
-    {
-        
-        return $this->name;
-    }
-
-    /**
-     * @return Collection|Item[]
-     */
-    public function getItems(): Collection
-    {
-        return $this->items;
-    }
-
-    public function addItem(Item $item): self
-    {
-        if (!$this->items->contains($item)) {
-            $this->items[] = $item;
-            $item->setCollection($this);
-        }
-
-        return $this;
-    }
-
-    public function removeItem(Item $item): self
-    {
-        if ($this->items->removeElement($item)) {
-            // set the owning side to null (unless already changed)
-            if ($item->getCollection() === $this) {
-                $item->setCollection(null);
-            }
-        }
 
         return $this;
     }

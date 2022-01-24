45dd5ad785646765d526b23098478523cc004523
VikaShepilo <shepilovika@gmail.com>
2022-01-24 21:52:26 +0300

    Fix: listItem

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -3,18 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\Item;
+use PhpParser\ErrorHandler\Collecting;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ListController extends AbstractController
-{
+{   
+    
     #[Route('/list', name: 'list')]
     public function index(Request $request)
     {   
         $user = $this->getUser();
-
-        $items = $this->getDoctrine()->getRepository(Item::class)->findAll();
+        $em = $this->getDoctrine()->getManager();
+        $items = $em->getRepository(Item::class)->findBy(array(), array('name_item' => 'ASC'));
 
         return $this->render('list/index.html.twig', [
             'items' => $items,
